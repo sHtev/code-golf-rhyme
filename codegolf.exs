5b1308a8c0319af7cf348dbf9b40c6d5691d07b6
Steve Gilroy <stephen.gilroy@isotoma.com>
2016-04-13 20:29:05 +0100

    full working first attempt

--- codegolf.exs
+++ codegolf.exs
@@ -1,8 +1,14 @@
-words = %{1 => "Head,", 2 => "shoulders,", 3 => "knees", 4 => "and", 5 => "toes", 6 => "eyes", 7 => "ears", 8 => "mouth", 9 => "nose"}
+w = ["Head,", "shoulders,", "knees and", "toes", "and eyes", "and ears", "and mouth and", "nose"]
+r = ["...,", "...,", "... ...", "...", "... ...", "... ...", "... ... ...", "..."]
 
-lines = [[1,2,3,4,5], [3,4,5], [1,2,3,4,5], [3,4,5], [4,6,4,7,4,8,4,9], [1,2,3,4,5], [3,4,5]]
+lines = [[1,2,3,4], [3, 4], [1,2,3,4], [3, 4], [5, 6, 7, 8], [1,2,3,4], [3,4]]
 
 defmodule CodeGolf do
+
+def gen_words(_, _, []), do: []
+def gen_words(_, 0, rest), do: rest
+def gen_words([replacement| r_rest], n, [_|rest]), do:
+    [replacement | gen_words(r_rest, n-1, rest)]
 
 def print_line([index | []], words),    do:  words[index]
 def print_line([index | rest], words),         do:  
@@ -12,7 +18,16 @@ def print_verse([line | []], words),     do: print_line(line, words)
 def print_verse([line | rest], words), do: 
         print_line(line, words) <> "\n" <> print_verse(rest, words)
 
+
+def print_song(lines, replace, words, 0),        do:
+    print_verse(lines, Enum.into(Enum.zip([1,2,3,4,5,6,7,8], gen_words(replace, 8, words)), %{}))
+
+def print_song(lines, replace, words, n),        do:
+    print_verse(lines, Enum.into(Enum.zip([1,2,3,4,5,6,7,8], gen_words(replace, 8-n, words)), %{})) <> "\n\n" <> print_song(lines, replace, words, n-1)
+
 end
 
 
-IO.puts CodeGolf.print_verse(lines, words)
+
+
+IO.puts CodeGolf.print_song(lines, r, w, 8)
